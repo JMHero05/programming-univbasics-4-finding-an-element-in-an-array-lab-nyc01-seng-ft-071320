0e56b795a27d992bd610bf0c2b02f7c6eebeea3e
Jake Mills <jake.mills.05@gmail.com>
2020-07-02 02:14:53 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,7 @@
 # 1. takes in two parameters, an array of integers and a value to find
 # 2. This method should return the index of the value that was passed in.
 # 3. If the value is not found, this method should return nil.
-#
+# 
 # NOTE: loop over the provided array and compare each value in the array to the value provided.
 # Remember though, that this is not the value that needs to be returned.
 
@@ -10,7 +10,7 @@ def find_element_index(array, value_to_find)
   count = 0
   while count < array.length do
     if array.include?(value_to_find)
-      return array[value_to_find]
+      return count
     else
       return nil
     end
